782114d9d1c338bcd9c60ca410f011305cf49a22
Yianni Ververis <yiannisver@gmail.com>
2018-08-07 12:30:30 -0400

    fix bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,17 @@ import QdtComponent from './QdtComponent';
 const viz = {
   type: 'QdtViz',
   props: {
-    type: 'barchart', id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', width: '300px', height: '300px',
+    type: 'barchart', id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', width: '100%', height: '300px',
   },
 };
 
 const App = () => (
-  <div>
-    <QdtComponent type={viz.type} props={viz.props} />
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <QdtComponent type={viz.type} props={viz.props} />
+      </div>
+    </div>
   </div>
 );
 
